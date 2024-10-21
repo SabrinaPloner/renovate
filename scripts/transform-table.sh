3936dbbff9d25b19327b12edbf7c6c71e51103da
Sabrina Ploner <sabrina.ploner@gepardec.com>
2024-10-21 10:07:59 +0200

    refactor custom datasource

--- scripts/transform-table.sh
+++ scripts/transform-table.sh
@@ -2,8 +2,8 @@
 
 awk '{print $1,$3}' grype_output.txt | tail -n +2 > tmp.txt
 
-mkdir -p packages
-rm -rf packages/*
+mkdir -p dependencies
+rm -rf dependencies/*
 
 
 
@@ -12,14 +12,14 @@ while read l; do
   packagename=$(echo $l | awk '{print $1}')
   fixedVersion=$(echo $l | awk '{print $2}')
 
-  mkdir packages/$packagename
-  touch packages/$packagename/versiontracker.json
-  echo "$fixedVersion"  >> "packages/$packagename/versiontracker.json"
+  mkdir dependencies/$packagename
+  touch dependencies/$packagename/versiontracker.json
+  echo "$fixedVersion"  >> "dependencies/$packagename/versiontracker.json"
 done <tmp.txt
 rm tmp.txt
 
 # only write highest version into file
-for packagedir in packages/*; do
+for packagedir in dependencies/*; do
   version=$(sort $packagedir/versiontracker.json | tail -1)
 
   echo "[{\"version\": \"$version\",\"filelink\": \"https://repo1.maven.org/maven2/junit/junit/4.13.1/junit-4.13.1.pom\"}]" > $packagedir/versiontracker.json
